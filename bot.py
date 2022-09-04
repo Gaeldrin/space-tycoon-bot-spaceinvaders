44514dd99a3520d309b14a4432692eb540b2d17e
Jakub Vonšovský <jakub.vonsovsky@aimsapi.com>
2022-09-05 01:25:34 +0200

    We send attacking fighters to enemy motherships

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ from space_tycoon_client.models.data import Data
 from space_tycoon_client.models.destination import Destination
 from space_tycoon_client.models.end_turn import EndTurn
 from space_tycoon_client.models.move_command import MoveCommand
+from space_tycoon_client.models.construct_command import ConstructCommand
+from space_tycoon_client.models.attack_command import AttackCommand
 from space_tycoon_client.models.player import Player
 from space_tycoon_client.models.player_id import PlayerId
 from space_tycoon_client.models.ship import Ship
@@ -38,6 +40,10 @@ class Game:
         self.data: Data = self.client.data_get()
         self.season = self.data.current_tick.season
         self.tick = self.data.current_tick.tick
+
+        self.attack_fighters = []
+        self.defend_fighters = []
+
         # this part is custom logic, feel free to edit / delete
         if self.player_id not in self.data.players:
             raise Exception("Logged as non-existent player")
@@ -72,24 +78,88 @@ class Game:
                 print(f"!!! EXCEPTION !!! Game logic error {e}")
                 traceback.print_exception(e)
 
+    def _get_fighters(self):
+        my_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                                self.data.ships.items() if ship.player == self.player_id and ship.ship_class == "4"}
+
+        return len(my_ships.keys()), my_ships
+
+    def _get_free_fighters(self):
+        my_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in self.data.ships.items()
+                                if ship.player == self.player_id and ship.ship_class == "4" and ship.command is None}
+        #my_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in self.data.ships.items()}
+
+        return len(my_ships.keys()), my_ships
+
+    def _get_enemy_duck_motherships(self) -> dict:
+        ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                             self.data.ships.items() if ship.player == "5" and ship.ship_class == "1"}
+
+        return ships
+
+    def _get_enemy_motherships(self) -> dict:
+        ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                                self.data.ships.items() if ship.player != self.player_id and ship.ship_class == "1"}
+
+        return ships
+
+    def _get_enemy_ships(self) -> dict:
+        ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
+                             self.data.ships.items() if ship.player != self.player_id and ship.ship_class == "1"}
+
+        return ships
+
+    def _get_our_mothership(self) -> dict:
+        ships: Dict[Ship] = [(ship_id, ship) for ship_id, ship in
+                                self.data.ships.items() if ship.player == self.player_id and ship.ship_class == "1"]
+
+        if len(ships) == 0:
+            return 0
+        return ships[0]
+
+    def _get_dist(self, x1, y1, x2, y2):
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+    def _get_closest_ship_to_all_fighters(self, enemy_ships, our_ships):
+        ship_pos = [(ship.position[0], ship.position[1]) for ship in our_ships.values()]
+
+        closest = 1e6
+        closest_id = None
+        for enemy_ship_id, enemy_ship in enemy_ships.items():
+            sum_dist = 0
+            for pos in ship_pos:
+                sum_dist += (pos[0], pos[1], enemy_ship.position[0], enemy_ship.position[1])
+            if sum_dist < closest:
+                closest = sum_dist
+                closest_id = enemy_ship_id
+
+        return closest_id
+
     def game_logic(self):
         # todo throw all this away
         self.recreate_me()
-        my_ships: Dict[Ship] = {ship_id: ship for ship_id, ship in
-                                self.data.ships.items() if ship.player == self.player_id}
-        ship_type_cnt = Counter(
-            (self.static_data.ship_classes[ship.ship_class].name for ship in my_ships.values()))
-        pretty_ship_type_cnt = ', '.join(
-            f"{k}:{v}" for k, v in ship_type_cnt.most_common())
-        print(f"I have {len(my_ships)} ships ({pretty_ship_type_cnt})")
 
+        fighter_count, fighters = self._get_free_fighters()
+        enemy_duck_motherships = self._get_enemy_duck_motherships()
+        enemy_motherships = self._get_enemy_motherships()
+        enemy_ships = self._get_enemy_ships()
+        our_mother_id, _ = self._get_our_mothership()
         commands = {}
-        for ship_id, ship in my_ships.items():
-            if ship.command is not None:
-                continue
-            random_planet_id = random.choice(list(self.data.planets.keys()))
-            print(f"sending {ship_id} to {self.data.planets[random_planet_id].name}({random_planet_id})")
-            commands[ship_id] = MoveCommand(type="move", destination=Destination(target=random_planet_id))
+
+        # attackers
+        if our_mother_id > 0:
+            for i in range(10 - fighter_count):
+                commands[our_mother_id] = ConstructCommand(ship_class="4")
+
+        if len(enemy_duck_motherships.keys()) > 0:
+            attack_id = self._get_closest_ship_to_all_fighters(enemy_duck_motherships, fighters)
+        if len(enemy_motherships.keys()) > 0:
+            attack_id = self._get_closest_ship_to_all_fighters(enemy_motherships, fighters)
+        elif len(enemy_ships.keys()) > 0:
+            attack_id = self._get_closest_ship_to_all_fighters(enemy_ships, fighters)
+
+        for f_id, fighter in fighters.items():
+            commands[f_id] = AttackCommand(attack_id)
 
         pprint(commands) if commands else None
         try:
