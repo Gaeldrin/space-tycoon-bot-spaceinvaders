311a760d7f4e937430ed0b9215433403ee7f36b0
David Vávra <david@vavra.me>
2022-09-06 00:26:44 +0200

    Cleanup

--- .github/workflows/deploy-bot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd space-tycoon-bot-spaceinvaders
-echo "Updating from git"
-git pull
-echo "Killing previous bot"
-pkill python3
-echo "Launching new bot in background"
-python3 bot.py > /dev/null &
-echo "Bot running"
